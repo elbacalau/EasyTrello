4508c52cd5d1610b4caba7d2c6aad1b613bdaac6
oriolreverte36 <oreverte@avanzaprl.es>
2025-01-04 18:04:47 +0100

    Add BoardController, IBoardService interface, and BoardService class for board management functionality

---
+++ backend/src/Controllers/BoardController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.src.Controllers
+{
+    [ApiController]
+    [Route("api/board")]
+    public class BoardController
+    {
+
+        public static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
