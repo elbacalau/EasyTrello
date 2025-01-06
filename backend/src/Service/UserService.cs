83a63f733708e02cccd92ff11c7f7230ebcd9d8b
oriolreverte36 <oreverte@avanzaprl.es>
2025-01-06 17:47:20 +0100

    Enhance UserResponseDTO to include Role property; update UserService to retrieve user roles from BoardUser relationships

--- backend/src/Service/UserService.cs
+++ backend/src/Service/UserService.cs
@@ -4,13 +4,15 @@ using backend.Models;
 using backend.src.DTOs;
 using backend.src.Infrastructure.Helpers;
 using backend.src.Interfaces;
+using backend.src.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.src.Service
 {
     public class UserService(AppDbContext context, Functions functions, IMapper mapper) : IUserService
     {
-        
+
         // DI
         private readonly AppDbContext _context = context;
         private readonly Functions _functions = functions;
@@ -18,10 +20,9 @@ namespace backend.src.Service
 
         public async Task<User> CreateUserAsync(User user)
         {
-            if (string.IsNullOrEmpty(user.Password) ||user.Password.Length < 6)
-            {
+            if (string.IsNullOrEmpty(user.Password) || user.Password.Length < 6)
                 throw new ArgumentNullException("Password must be at least 6 characters long");
-            }
+            
             try
             {
                 user.Password = _functions.EncryptPassword(user.Password);
@@ -31,17 +32,24 @@ namespace backend.src.Service
                 Console.WriteLine($"Error: {ex.Message}");
                 throw;
             }
-            
-            _context.Users.Add(user);  
+
+            _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;  
+            return user;
         }
 
         public async Task<UserResponse> GetUserByIdAsync(int id)
         {
             User user = await _context.Users.FindAsync(id) ?? throw new ArgumentException("User not found");
 
-            return _mapper.Map<UserResponse>(user);
+            BoardUser boardUser = await _context.BoardUsers.FirstOrDefaultAsync(bu => bu.UserId == id) ?? throw new ArgumentException("User not found");
+
+            string role = Enum.GetName(typeof(BoardRole), boardUser.Role) ?? throw new ArgumentException("Invalid Role"); ;
+        
+            UserResponse userResponse = _mapper.Map<UserResponse>(user);
+            userResponse.Role = role;
+
+            return userResponse;
         }
     }
 }
