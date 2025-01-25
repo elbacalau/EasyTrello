925fb798eb60d8a6a979e8b6b045618dc763b8e6
oriolreverte36 <oreverte@avanzaprl.es>
2025-01-25 19:49:03 +0100

    Update CSS and refactor Login component: add color scheme support, update PostCSS version, and modify component imports

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,18 @@
 
-export const Login = () => {
+import { Description, Field, Label } from "../../components/fieldset";
+import { Input } from "../../components/input";
+
+
+function Login() {
   return (
-    <div>Login</div>
-  )
+    <Field>
+      <Label>Product name</Label>
+      <Description>
+        Use the name you'd like people to see in their cart.
+      </Description>
+      <Input name="product_name" />
+    </Field>
+  );
 }
+
+export default Login;
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "../pages/Login";
+import Login  from "../pages/Login";
 import { Dashboard } from "../pages/Dashboard";
 import PrivateRoute from "../components/PrivateRoute";
 
